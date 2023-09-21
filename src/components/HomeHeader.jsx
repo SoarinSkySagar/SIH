38a71b66476335473608889cef57106ac8500d82
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2023-09-21 16:45:50 +0530

    Update HomeHeader.jsx

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -5,9 +5,10 @@ export default function HomeHeader() {
     <header className='w-96vw overflow-hidden h-90vh z-100' >
         <div className="w-11/12 my-0 mx-auto grid grid-cols-2 items-center gap-x-20 h-full mt-8">
             <div class="my-4 pt-6 pb-12">
-                <h1 className='text-8xl font-bold mb-8'>Lorem ipsum dolorum</h1>
+                <h1 className='text-8xl font-bold mb-8'>Generate valid certificates with Blockcertify</h1>
                 <p className='my-4 pb-9 text-2xl mb-8'>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga eum debitis, deleniti reprehenderit tenetur quidem repellendus eius veniam aspernatur fugit quia deserunt corrupti at dicta. Id consectetur porro doloremque provident!
+                Currently large no of training programs is organized, and certificates are provided. 
+                There is no mechanism to validate digital certificates so here we are creating a system in which custom digital certificate can be generated!
                 </p>
                 <a href="/" class="bg-[#5967E8] font-semibold px-12 py-4 text-3xl rounded-full ">Get Started   <span>&#8594;</span></a>
             </div>
